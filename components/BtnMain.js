02cf26f9dd32b4a47a864bab0adf2ae3535cc9ac
Artem Nazarchenko <tiymanazar62@gmail.com>
2024-02-04 21:47:15 +0200

    made btn change size and vibrate on click

--- components/BtnMain.js
+++ components/BtnMain.js
@@ -2,8 +2,56 @@
 export default class BtnMain {
     constructor(){
         this.btnMainHtml = '<div class="btn-main"><span class="summ">90</span></div>';
+        this.btnMainElem = null;
+        this.isClicked = false;
+        this.isSent = false;
     }
     render(){
         document.querySelector('.app__wrapper').insertAdjacentHTML('beforeend', this.btnMainHtml);
+        this.btnMainElem = document.querySelector('.btn-main');
+        this.handleEvents();
+    }
+
+    handleEvents(){
+        this.btnMainElem.addEventListener('touchstart', this.handleTouchStart);
+        this.btnMainElem.addEventListener('touchend', this.handleTouchEnd);
+    }
+    
+    handleTouchStart(e){
+        this.isSent = false;
+        this.isClicked = true;
+        console.log('hello');
+        e.target.classList.add('btn-main--active');
+
+        setTimeout(() => {
+            if(this.isClicked){
+                if(!this.isSent){
+                    // vibrate(5000);
+                    console.log('ok')
+                    this.isSent = true;
+                }
+            }
+        }, 3000)
+    }
+    
+    handleTouchEnd(e){
+        this.isClicked = false;
+        e.target.removeEventListener('touchstart', this.handleTouchStart);
+        e.target.classList.remove('btn-main--active');
+    }
+
+
+    vibrate(duration) {
+        if ("vibrate" in navigator) {
+            const pulses = Math.floor(duration / 100);
+            const pattern = [];
+            for (let i = 0; i < pulses; i++) {
+                pattern.push(50); // Время вибрации
+                pattern.push(50); // Время паузы между вибрациями
+            }
+            navigator.vibrate(pattern);
+        } else {
+            alert("Ваше устройство не поддерживает API вибрации.");
+        }
     }
 }
